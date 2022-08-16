2fb623197bf2577f55418c873bc29e759d9e755e
oleg-torba <100319055+oleg-torba@users.noreply.github.com>
2022-08-16 22:48:13 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,40 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 
-import SimpleLightBox from 'simplelightbox;
-import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightBox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-
-const galleryBox = document.querySelector(".gallery");
+const galleryBox = document.querySelector('.gallery');
 const galleryList = createGalleryLightBox(galleryItems);
-galleryBox.insertAdjacentHTML("beforeend", galleryList);
+galleryBox.insertAdjacentHTML('beforeend', galleryList);
 
 function createGalleryLightBox(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
       return `
-    <div class="gallery__item">
-    <a class="gallery__link" href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
-</div>`;
+    <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
     })
-    .join("");
+    .join('');
 }
-galleryBox.addEventListener("click", onGalleryLightBoxClick);
-function onGalleryLightBoxClick(e) {
-  e.preventDefault();
-  const imgSource = e.target.dataset.source;
-  const instance = basicLightbox.create(`
-    <img
-    src="${imgSource}"
-    >
-`);
 
-  instance.show();
-}
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionPosition: 'top',
+  captionsData: 'alt',
+  captionDelay: 250,
+  enableKeyboard: true,
+  close: false,
+  fadeSpeed: 300,
+  overlayOpacity: 0.5,
+});
