706e34b66850b8840850996014903404e60647c6
oleg-torba <100319055+oleg-torba@users.noreply.github.com>
2022-08-16 22:33:51 +0300

    gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,40 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+import SimpleLightBox from 'SimpleLightBox;
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+const galleryBox = document.querySelector(".gallery");
+const galleryList = createGalleryLightBox(galleryItems);
+galleryBox.insertAdjacentHTML("beforeend", galleryList);
+
+function createGalleryLightBox(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+galleryBox.addEventListener("click", onGalleryLightBoxClick);
+function onGalleryLightBoxClick(e) {
+  e.preventDefault();
+  const imgSource = e.target.dataset.source;
+  const instance = basicLightbox.create(`
+    <img
+    src="${imgSource}"
+    >
+`);
+
+  instance.show();
+}
