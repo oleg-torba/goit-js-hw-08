a9a97a945d6903f37eacc0832074050fecb09975
oleg-torba <100319055+oleg-torba@users.noreply.github.com>
2022-08-17 00:01:08 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,3 +3,9 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
+
+const onPlay = function(timeUpdate){
+ currentTime, throttle = 1s
+}
+
+player.on('play', onPlay);
