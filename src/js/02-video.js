e311cbf367729b2660adb6b5787e74494552ebc3
oleg-torba <100319055+oleg-torba@users.noreply.github.com>
2022-08-17 23:51:51 +0300

    03

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,11 +1,11 @@
-import videoPlayer from '@vimeo/player';
+//import videoPlayer from '@vimeo/player';//
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
-const onPlay = function(timeUpdate){
- currentTime, throttle = 1s
-}
+const onPlay = function (timeUpdate) {
+  currentTime, throttle(timeUpdate, 1000);
+};
 
 player.on('play', onPlay);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,22 @@
+const form = document.querySelector('.feedback-form');
+const textArea = document.querySelector('.feedback-form textarea');
+const email = document.querySelector('.feedback-form input');
 
+form.addEventListener('submit', onFormSubmit);
+textArea.addEventListener('input', onTextAreaInput);
+email.addEventListener('input', onTextAreaInput);
+populateTextArea();
+
+function onTextAreaInput(evt) {
+  const message = evt.currentTarget.value;
+  localStorage.setItem('feedback-form-state', message);
+  console.log(message);
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+}
+
+function populateTextArea() {
+  const saveMessage = localStorage.getItem('feedback-from-state');
+}
