1ce08321c74bef2e1f18fffd8b7d8cb80c701428
oleg-torba <100319055+oleg-torba@users.noreply.github.com>
2022-08-20 23:17:58 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,11 +1,21 @@
-//import videoPlayer from '@vimeo/player';//
 import throttle from 'lodash.throttle';
+import vimeoPlayer from '@vimeo/player';
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new vimeoPlayer(iframe);
 
-const onPlay = function (timeUpdate) {
-  currentTime, throttle(timeUpdate, 1000);
+const STORAGE_KEY = 'videoplayer-current-time';
+
+function data({ seconds }) {
+  localStorage.setItem(STORAGE_KEY, seconds);
+}
+
+player.on('timeupdate', throttle(data, 1000));
+
+const dataStorage = function () {
+  localStorage.getItem(STORAGE_KEY);
+
+  if (dataStorage) {
+    player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
+  }
 };
-
-player.on('play', onPlay);
